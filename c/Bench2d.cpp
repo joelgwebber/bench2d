078bbe795ef6923f3736bb9bb4219bb164ec91d5
Joel Webber <jgw@google.com>
2011-12-13 11:54:24 -0500

    Fixed makefiles, removed copy of hello from Bench2d.cpp. Confirmed that emscripten compile works fine on Linux.

--- c/Bench2d.cpp
+++ c/Bench2d.cpp
@@ -5,11 +5,13 @@
 
 using namespace std;
 
-#if 1
-#define WARMUP 0
+// Turn this on to include the y-position of the top box in the output.
+#define DEBUG 0
+
+#define WARMUP 64
 #define FRAMES 256
 
-const int e_count = 2;
+const int e_count = 40;
 
 void bench() {
 	// Define the gravity vector.
@@ -28,7 +30,7 @@ void bench() {
 		ground->CreateFixture(&shape, 0.0f);
 	}
 
-b2Body* topBody;
+  b2Body* topBody;
 
 	{
 		float32 a = 0.5f;
@@ -50,7 +52,7 @@ b2Body* topBody;
 				b2Body* body = world.CreateBody(&bd);
 				body->CreateFixture(&shape, 5.0f);
 
-topBody = body;
+        topBody = body;
 
 				y += deltaY;
 			}
@@ -69,7 +71,9 @@ topBody = body;
 		world.Step(1.0f/60.0f, 3, 3);
 		clock_t end = clock();
 		times[i] = end - start;
-printf("%f :: ", topBody->GetPosition().y);
+#if DEBUG
+    printf("%f :: ", topBody->GetPosition().y);
+#endif
 		printf("%f\n", (float32)(end - start) / CLOCKS_PER_SEC * 1000);
 	}
 
@@ -86,86 +90,4 @@ int main(int argc, char** argv) {
   bench();
   return 0;
 }
-
-#else
-
-int main(int argc, char** argv)
-{
-	B2_NOT_USED(argc);
-	B2_NOT_USED(argv);
-
-	// Define the gravity vector.
-	b2Vec2 gravity(0.0f, -10.0f);
-
-	// Construct a world object, which will hold and simulate the rigid bodies.
-	b2World world(gravity);
-
-	// Define the ground body.
-	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(0.0f, -10.0f);
-
-	// Call the body factory which allocates memory for the ground body
-	// from a pool and creates the ground box shape (also from a pool).
-	// The body is also added to the world.
-	b2Body* groundBody = world.CreateBody(&groundBodyDef);
-
-	// Define the ground box shape.
-	b2PolygonShape groundBox;
-
-	// The extents are the half-widths of the box.
-	groundBox.SetAsBox(50.0f, 10.0f);
-
-	// Add the ground fixture to the ground body.
-	groundBody->CreateFixture(&groundBox, 0.0f);
-
-	// Define the dynamic body. We set its position and call the body factory.
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, 4.0f);
-	b2Body* body = world.CreateBody(&bodyDef);
-
-	// Define another box shape for our dynamic body.
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox(1.0f, 1.0f);
-
-	// Define the dynamic body fixture.
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
-
-	// Set the box density to be non-zero, so it will be dynamic.
-	fixtureDef.density = 1.0f;
-
-	// Override the default friction.
-	fixtureDef.friction = 0.3f;
-
-	// Add the shape to the body.
-	body->CreateFixture(&fixtureDef);
-
-	// Prepare for simulation. Typically we use a time step of 1/60 of a
-	// second (60Hz) and 10 iterations. This provides a high quality simulation
-	// in most game scenarios.
-	float32 timeStep = 1.0f / 60.0f;
-	int32 velocityIterations = 6;
-	int32 positionIterations = 2;
-
-	// This is our little game loop.
-	for (int32 i = 0; i < 60; ++i)
-	{
-		// Instruct the world to perform a single step of simulation.
-		// It is generally best to keep the time step and iterations fixed.
-		world.Step(timeStep, velocityIterations, positionIterations);
-
-		// Now print the position and angle of the body.
-		b2Vec2 position = body->GetPosition();
-		float32 angle = body->GetAngle();
-
-		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-	}
-
-	// When the world destructor is called, all bodies and joints are freed. This can
-	// create orphaned pointers, so be careful about your world management.
-
-	return 0;
-}
-#endif
 
