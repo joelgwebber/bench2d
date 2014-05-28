15be49ce87536cd33554738233f58b6dcd1da742
Ray Cromwell <cromwellian@google.com>
2014-05-28 13:36:01 -0700

    Checkin custom gwt build

--- java/compile.sh
+++ java/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -cp /Users/cromwellian/work/gwt/build/staging/gwt-0.0.0/gwt-dev.jar:/Users/cromwellian/work/gwt/build/staging/gwt-0.0.0/gwt-user.jar:/Users/cromwellian/work/bench2d/java/src:/Users/cromwellian/work/bench2d/java/gwt:/Users/cromwellian/work/bench2d/java/lib/jbox2d-library-2.2.1.2-SNAPSHOT-jar-with-dependencies.jar:/Users/cromwellian/work/bench2d/java/lib/jbox2d-library-2.2.1.2-SNAPSHOT-sources.jar  com.google.gwt.dev.Compiler -XdisableCastChecking  -optimize 10 -XenableClosureCompiler -style PRETTY j15r.Bench2d
+java -cp ./lib/gwt-dev.jar:./lib/gwt-user.jar:./src:./gwt:./lib/jbox2d-library-2.2.1.2-SNAPSHOT-jar-with-dependencies.jar:./lib/jbox2d-library-2.2.1.2-SNAPSHOT-sources.jar  com.google.gwt.dev.Compiler -XdisableCastChecking  -optimize 10 -XenableClosureCompiler -style PRETTY j15r.Bench2d
 
